e468dfd292f3bc3df1fb13a1e4d46b8fc0423c7c
Raphael Freitas <dev.rapha@gmail.com>
2018-04-18 22:54:14 -0300

    cors config

--- src/main/java/com/freitas/todoservice/config/CorsFilter.java
+++ src/main/java/com/freitas/todoservice/config/CorsFilter.java
@@ -11,6 +11,8 @@ public class CorsFilter implements WebFilter {
 	@Override
 	public Mono<Void> filter(ServerWebExchange server, WebFilterChain web) {
 		server.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
+		server.getResponse().getHeaders().add("Access-Control-Allow-Methods", "*");
+		server.getResponse().getHeaders().add("Access-Control-Allow-Headers", "Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
 		return web.filter(server);
 	}
 
